997332ee7f0541e479d364aa2d2395b37c3e6e60
rainie_pang <1838529223@qq.com>
2020-02-08 11:41:47 +0800

    official_管理资源

--- Official_Learn/webpack.config.js
+++ Official_Learn/webpack.config.js
@@ -1,9 +1,46 @@
 const path = require('path');
-
+const TerserJSPlugin = require("terser-webpack-plugin");
+const MinCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MinCssExtractPlugin.loader, //(This MinCssExtractPlugin plugin should be used only on production builds without style-loader in the loaders chain, especially if you want to have HMR in development.)
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new MinCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ],
+    optimization: {
+        minimizer: [
+            new TerserJSPlugin({}),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     }
 }
