3528a387ffa8cdfe24599e201c5148499c18a0fa
rainie_pang <1838529223@qq.com>
2020-02-08 11:51:04 +0800

    official_ revert start

--- Official_Learn/src/index.js
+++ Official_Learn/src/index.js
@@ -1,16 +1,8 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './rainie.jpg';
 
 function component(){
     let element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-
     return element;
 }
 
