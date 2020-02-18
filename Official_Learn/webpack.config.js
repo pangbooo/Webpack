4caf7a4b23ba883def7520156caa0f0c5411f83e
pangbo <dhc-bo.pang@dhc.com.cn>
2020-02-18 10:39:34 +0800

    official_开发环境

--- Official_Learn/webpack.config.js
+++ Official_Learn/webpack.config.js
@@ -2,17 +2,23 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index.js',
         print: './src/print.js'
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '管理输出111'
+            title: '开发环境'
         })
-    ]
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    }
 }
