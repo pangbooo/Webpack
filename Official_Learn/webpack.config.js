5742e313ec77aaa27376e56eed239633fb07c89b
pangbo <pangbo@rakuten.dhc.com>
2020-11-26 16:55:06 +0800

    缓存（提取引导模板(extracting boilerplate)）

--- Official_Learn/webpack.config.js
+++ Official_Learn/webpack.config.js
@@ -33,5 +33,16 @@ module.exports = {
         // fix Cannot use [chunkhash] or [contenthash] for chunk in '[name].[chunkhash].js' (use [hash] instead) 
         // new webpack.HotModuleReplacementPlugin()
     ],
-
+    optimization: {
+        runtimeChunk: 'single', //runtime 代码拆分为一个单独的 chunk。将其设置为 single 来为所有 chunk 创建一个 runtime bundle
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    }
 }
